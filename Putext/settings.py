26b210dc40e09b51b34867c3b4c66e61adb14419
firedranzer <rupeshharode@gmail.com>
2018-01-08 10:50:57 +0530

    Relocated html files

--- Putext/settings.py
+++ Putext/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Putext.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates/html')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
