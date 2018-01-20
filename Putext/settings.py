0232c2754ad4ff86b525025442ed4e32f672ec4c
firedranzer <rupeshharode@gmail.com>
2018-01-20 13:09:13 +0530

    Minor changes with static files

--- Putext/settings.py
+++ Putext/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "Putext/static"),
+    #'/var/www/static/',
 ]
--- Putext/urls.py
+++ Putext/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from home.views import home
 from .views import GeneratePDF
@@ -18,3 +20,6 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
